f8efbaf7a1cf752ae5cd3637f23df867c1b6e5d1
Stefan Starosta <stefan.starosta@gmail.com>
2023-03-11 11:58:14 +0100

    5 - Going Reactive: Declarative Pattern for Data retrieval

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -11,24 +11,19 @@ import {ProductService} from './product.service';
   styleUrls: ['./product-list.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ProductListComponent implements OnInit {
+export class ProductListComponent {
   pageTitle = 'Product List';
   errorMessage = '';
   categories: ProductCategory[] = [];
 
-  products$: Observable<Product[]> | undefined;
+  products$ = this.productService.products$.pipe(
+    catchError(err => {
+      this.errorMessage = err;
+      return EMPTY;
+    })
+  );
 
   constructor(private productService: ProductService) { }
-
-  ngOnInit(): void {
-    this.products$ = this.productService.getProducts()
-      .pipe(
-        catchError(err => {
-          this.errorMessage = err;
-          return EMPTY;
-        })
-      );
-  }
 
   onAdd(): void {
     console.log('Not yet implemented');
