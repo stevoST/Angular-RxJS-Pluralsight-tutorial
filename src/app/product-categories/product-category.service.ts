19ef331134952df0ab10c172a06dc8487535fa32
Stefan Starosta <stefan.starosta@gmail.com>
2023-03-13 20:53:02 +0100

    Combining Data Streams to Map an Id to a String:Demo

--- src/app/product-categories/product-category.service.ts
+++ src/app/product-categories/product-category.service.ts
@@ -1,7 +1,9 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-import { throwError, Observable } from 'rxjs';
+import {throwError, Observable, map, tap, catchError} from 'rxjs';
+import {Product} from "../products/product";
+import {ProductCategory} from "./product-category";
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +11,11 @@ import { throwError, Observable } from 'rxjs';
 export class ProductCategoryService {
   private productCategoriesUrl = 'api/productCategories';
 
+  productCategories$ = this.http.get<ProductCategory[]>(this.productCategoriesUrl)
+    .pipe(
+      tap(data => console.log('categories', JSON.stringify(data))),
+      catchError(this.handleError)
+    );
   constructor(private http: HttpClient) { }
 
   private handleError(err: HttpErrorResponse): Observable<never> {
--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -1,9 +1,10 @@
 import {Injectable} from '@angular/core';
 import {HttpClient, HttpErrorResponse} from '@angular/common/http';
 
-import {catchError, map, Observable, tap, throwError} from 'rxjs';
+import {catchError, combineLatest, map, Observable, tap, throwError} from 'rxjs';
 
 import {Product} from './product';
+import {ProductCategoryService} from "../product-categories/product-category.service";
 
 @Injectable({
   providedIn: 'root'
@@ -14,17 +15,25 @@ export class ProductService {
 
   products$ = this.http.get<Product[]>(this.productsUrl)
     .pipe(
-      map(products =>
-      products.map(product => ({
-        ...product,
-        price: product.price ? product.price * 1.5 : 0,
-        // searchKey: [product.searchKey] // does not work with this
-      } as Product))),
       tap(data => console.log('Products: ', JSON.stringify(data))),
       catchError(this.handleError)
     );
 
-  constructor(private http: HttpClient) {
+  productsWithCategory$ = combineLatest([
+    this.products$,
+    this.productCategoryService.productCategories$
+  ]).pipe(
+    map(([products, categories]) =>
+      products.map(product => ({
+        ...product,
+        price: product.price ? product.price * 1.5 : 0,
+        // searchKey: [product.searchKey] // does not work with this
+        category: categories.find(c => product.categoryId === c.id)?.name
+      } as Product))),
+  )
+
+  constructor(private http: HttpClient,
+              private productCategoryService: ProductCategoryService) {
   }
 
   private fakeProduct(): Product {
